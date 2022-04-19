21abdc1bead66ffb4a929e904e75869071182c8d
Assis Zang <assis.compasso@dotzmkt.com.br>
2022-04-19 15:14:10 -0300

    feat: Project creation

---
+++ ChatSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace ChatSignalR.Hubs;
+
+public class ChatHub : Hub
+{
+    public async Task SendMessage(string user, string message) =>
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+}
